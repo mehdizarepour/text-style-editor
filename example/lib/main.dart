ad036f9fe7d0821caf290e4ce1e4e01bdb75c936
Mehdi Zarepour <mehdi.zarepoor@gmail.com>
2020-08-08 15:40:36 +0430

    Add example project

---
+++ example/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+// import 'package:text_style_editor/text_style_editor.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TextStyleEditor Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'TextStyleEditor Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text('TextStyleEditor Demo'),
+      ),
+    );
+  }
+}
--- lib/text_style_editor.dart
+++ lib/text_style_editor.dart
@@ -1,7 +1,37 @@
 library text_style_editor;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomAlertBox {
+  /// Bu şekilde döküman yorumları oluşturabilirsiniz kullanan kişiler için faydalı olur.
+  static Future showCustomAlertBox({
+    @required BuildContext context,
+    @required Widget willDisplayWidget,
+  }) {
+    assert(context != null, "context is null!!");
+    assert(willDisplayWidget != null, "willDisplayWidget is null!!");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
 }
