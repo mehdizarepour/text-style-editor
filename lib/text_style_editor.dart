5f03525b81735b275121c96dc3cdde89040aa717
Mehdi Zarepour <mehdi.zarepoor@gmail.com>
2021-04-26 12:08:04 +0430

    Select tools option

--- lib/text_style_editor.dart
+++ lib/text_style_editor.dart
@@ -6,34 +6,50 @@ import 'src/font_family_tool.dart';
 import 'src/text_format_tool.dart';
 import 'src/toolbar.dart';
 
-class TextStyleEditor extends StatelessWidget {
+class TextStyleEditor extends StatefulWidget {
+  @override
+  _TextStyleEditorState createState() => _TextStyleEditorState();
+}
+
+class _TextStyleEditorState extends State<TextStyleEditor> {
+  ToolbarAction _currentTool = ToolbarAction.editing;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Toolbar(
-          onToolSelect: (action) {},
+          onToolSelect: (action) => setState(() => _currentTool = action),
         ),
         Divider(),
         Expanded(
           child: SingleChildScrollView(
-            child: TextFormatTool(
-              onTextFormatEdited: (bold, italic, caps) {},
-              onTextAlignEdited: (align) {},
-            ),
-            // child: FontFamilyTool(
-            //   fonts: [
-            //     'Font1',
-            //     'Font2',
-            //     'Font3',
-            //     'Font4',
-            //     'Font5',
-            //     'Font6',
-            //     'Font7',
-            //     'Font8'
-            //   ],
-            //   onSelectFont: (fontFamily) {},
-            // ),
+            child: () {
+              // Choice tools
+              switch (_currentTool) {
+                case ToolbarAction.fontFamily:
+                  return FontFamilyTool(
+                    fonts: [
+                      'Font1',
+                      'Font2',
+                      'Font3',
+                      'Font4',
+                      'Font5',
+                      'Font6',
+                      'Font7',
+                      'Font8'
+                    ],
+                    onSelectFont: (fontFamily) {},
+                  );
+                case ToolbarAction.strikethrough:
+                  return TextFormatTool(
+                    onTextFormatEdited: (bold, italic, caps) {},
+                    onTextAlignEdited: (align) {},
+                  );
+                default:
+                  return Container();
+              }
+            }(),
           ),
         ),
       ],
