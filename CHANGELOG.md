b5ded42605126bf0973b29b000be8e47f3eaa3f5
Mehdi Zarepour <mehdi.zarepoor@gmail.com>
2021-05-14 12:29:29 +0430

    Add `inithialTool` prop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 2021-05-14
+
+- Add `inithialTool` prop
+
 ## [1.0.0] - 2021-05-03
 
 - New design & more features
--- lib/src/toolbar.dart
+++ lib/src/toolbar.dart
@@ -4,16 +4,27 @@ import 'option_button.dart';
 import 'toolbar_action.dart';
 
 class Toolbar extends StatefulWidget {
+  final EditorToolbarAction inithialTool;
   final Function(EditorToolbarAction) onToolSelect;
 
-  Toolbar({required this.onToolSelect});
+  Toolbar({
+    this.inithialTool = EditorToolbarAction.editor,
+    required this.onToolSelect,
+  });
 
   @override
   _ToolbarState createState() => _ToolbarState();
 }
 
 class _ToolbarState extends State<Toolbar> {
-  EditorToolbarAction _selectedAction = EditorToolbarAction.editor;
+  late EditorToolbarAction _selectedAction;
+  @override
+  void initState() {
+    _selectedAction = widget.inithialTool;
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
